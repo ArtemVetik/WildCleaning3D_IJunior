35f43fb0f24a88edbebf0a5767527363b418c49c
ArtemVetik <av.unitygames@gmail.com>
2021-01-06 17:53:15 +0300

    Added player characteristic editor

---
+++ Assets/Editor/Scripts/LevelRedactor/PlayerCharacteristicEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerCharacteristics))]
+public class PlayerCharacteristicEditor : Editor
+{
+    private PlayerCharacteristics _characteristics;
+    private bool _modifiedParametersFoldout;
+    private PlayerData _modifiedData;
+
+    private void OnEnable()
+    {
+        _characteristics = target as PlayerCharacteristics;
+
+        _modifiedData = new PlayerData();
+        _modifiedData.Load(new JsonSaveLoad());
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        _modifiedParametersFoldout = EditorGUILayout.Foldout(_modifiedParametersFoldout, "ModifiedParameters", true);
+        if (_modifiedParametersFoldout)
+        {
+            GUI.enabled = false;
+            var saveIndentLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel++;
+
+            EditorGUILayout.FloatField("Speed", _modifiedData.Speed);
+
+            EditorGUI.indentLevel = saveIndentLevel;
+            GUI.enabled = true;
+        }
+
+    }
+}
